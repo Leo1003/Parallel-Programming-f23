86e2cb1757a5651445ebb3cc9780e57424732b89
Leo Chen <leo881003@gmail.com>
2023-11-23 00:22:22 +0800

    HW3: Complete bottom-up BFS serial

--- HW3/part2/breadth_first_search/bfs.cpp
+++ HW3/part2/breadth_first_search/bfs.cpp
@@ -117,6 +117,62 @@ void bfs_bottom_up(Graph graph, solution *sol)
     // As was done in the top-down case, you may wish to organize your
     // code by creating subroutine bottom_up_step() that is called in
     // each step of the BFS process.
+    vertex_set list1;
+    vertex_set list2;
+    vertex_set_init(&list1, graph->num_nodes);
+    vertex_set_init(&list2, graph->num_nodes);
+
+    vertex_set *frontier = &list1;
+    vertex_set *new_frontier = &list2;
+
+    // initialize all nodes to NOT_VISITED
+    for (int i = 0; i < graph->num_nodes; i++)
+        sol->distances[i] = NOT_VISITED_MARKER;
+
+    // setup frontier with the root node
+    frontier->vertices[frontier->count++] = ROOT_NODE_ID;
+    sol->distances[ROOT_NODE_ID] = 0;
+
+    while (frontier->count > 0) {
+
+#ifdef VERBOSE
+        double start_time = CycleTimer::currentSeconds();
+#endif
+
+        vertex_set_clear(new_frontier);
+
+        for (int i = 0; i < num_nodes(graph); i++) {
+            if (sol->distances[i] == NOT_VISITED_MARKER) {
+                bool in_frontier = false;
+                const Vertex* start = incoming_begin(graph, i);
+                const Vertex* end = incoming_end(graph, i);
+                for (const Vertex* v = start; v != end; v++) {
+                    for (int f = 0; f < frontier->count; f++) {
+                        if (*v == frontier->vertices[f]) {
+                            in_frontier = true;
+                            int index = __sync_fetch_and_add(&new_frontier->count, 1);
+                            new_frontier->vertices[index] = i;
+                            sol->distances[i] = sol->distances[*v] + 1;
+                            break;
+                        }
+                    }
+                    if (in_frontier) {
+                        break;
+                    }
+                }
+            }
+        }
+
+#ifdef VERBOSE
+        double end_time = CycleTimer::currentSeconds();
+        printf("frontier=%-10d %.4f sec\n", frontier->count, end_time - start_time);
+#endif
+
+        // swap pointers
+        vertex_set *tmp = frontier;
+        frontier = new_frontier;
+        new_frontier = tmp;
+    }
 }
 
 void bfs_hybrid(Graph graph, solution *sol)
