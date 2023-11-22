42b72575bfabb9eb4a4b3798a6d90da14a1f0a42
Leo Chen <leo881003@gmail.com>
2023-11-22 21:23:48 +0800

    Complete single thread page_rank

--- HW3/part2/page_rank/page_rank.cpp
+++ HW3/part2/page_rank/page_rank.cpp
@@ -1,9 +1,11 @@
 #include "page_rank.h"
 
+#include <cstring>
 #include <stdlib.h>
 #include <cmath>
 #include <omp.h>
 #include <utility>
+#include <vector>
 
 #include "../common/CycleTimer.h"
 #include "../common/graph.h"
@@ -17,18 +19,7 @@
 //
 void pageRank(Graph g, double *solution, double damping, double convergence)
 {
-
-  // initialize vertex weights to uniform probability. Double
-  // precision scores are used to avoid underflow for large graphs
-
-  int numNodes = num_nodes(g);
-  double equal_prob = 1.0 / numNodes;
-  for (int i = 0; i < numNodes; ++i)
-  {
-    solution[i] = equal_prob;
-  }
-
-  /*
+    /*
      For PP students: Implement the page rank algorithm here.  You
      are expected to parallelize the algorithm using openMP.  Your
      solution may need to allocate (and free) temporary arrays.
@@ -36,24 +27,57 @@ void pageRank(Graph g, double *solution, double damping, double convergence)
      Basic page rank pseudocode is provided below to get you started:
 
      // initialization: see example code above
-     score_old[vi] = 1/numNodes;
+    */
+    const int numNodes = num_nodes(g);
+    bool converged = false;
 
-     while (!converged) {
+    // initialize vertex weights to uniform probability. Double
+    // precision scores are used to avoid underflow for large graphs
+    double *pr_t = (double *)malloc(sizeof(double) * numNodes);
+    double equal_prob = 1.0 / numNodes;
+    std::vector<Vertex> tail_nodes;
+    for (int i = 0; i < numNodes; ++i) {
+        pr_t[i] = equal_prob;
+        if (outgoing_size(g, i) == 0) {
+            tail_nodes.push_back(i);
+        }
+    }
 
-       // compute score_new[vi] for all nodes vi:
-       score_new[vi] = sum over all nodes vj reachable from incoming edges
-                          { score_old[vj] / number of edges leaving vj  }
-       score_new[vi] = (damping * score_new[vi]) + (1.0-damping) / numNodes;
+    while (!converged) {
+        fprintf(stderr, "pr_t[0] = %le\n", pr_t[0]);
+        double *pr_t1 = (double *)malloc(sizeof(double) * numNodes);
 
-       score_new[vi] += sum over all nodes v in graph with no outgoing edges
-                          { damping * score_old[v] / numNodes }
+        double tail_score = 0.0;
+        for (int i = 0; i < tail_nodes.size(); i++) {
+            tail_score += pr_t[tail_nodes[i]];
+        }
+        tail_score = tail_score * damping / numNodes;
 
-       // compute how much per-node scores have changed
-       // quit once algorithm has converged
+        for (int i = 0; i < numNodes; i++) {
+            double score = 0.0;
 
-       global_diff = sum over all nodes vi { abs(score_new[vi] - score_old[vi]) };
-       converged = (global_diff < convergence)
-     }
+            const Vertex* start = incoming_begin(g, i);
+            const Vertex* end = incoming_end(g, i);
+            for (const Vertex* j = start; j != end; j++) {
+                score += pr_t[*j] / outgoing_size(g, *j);
+            }
 
-   */
+            pr_t1[i] = (1.0 - damping) / numNodes + (damping * score) + tail_score;
+        }
+
+        // compute how much per-node scores have changed
+        // quit once algorithm has converged
+        double global_diff = 0.0;
+        for (int i = 0; i < numNodes; i++) {
+            global_diff += abs(pr_t1[i] - pr_t[i]);
+        }
+        converged = (global_diff < convergence);
+
+        free(pr_t);
+        pr_t = pr_t1;
+        pr_t1 = NULL;
+    }
+
+    memcpy(solution, pr_t, sizeof(double) * numNodes);
+    free(pr_t);
 }
