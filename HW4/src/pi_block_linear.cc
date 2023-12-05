48e9607996c566ca88dfab2aac3808859e8b8913
Leo Chen <leo881003@gmail.com>
2023-12-05 18:38:43 +0800

    HW4: pi_block_linear multithread

--- HW4/src/pi_block_linear.cc
+++ HW4/src/pi_block_linear.cc
@@ -1,6 +1,8 @@
-#include <cstdint>
+#include <sys/sysinfo.h>
 #define _GNU_SOURCE
 #include <mpi.h>
+#include <pthread.h>
+#include <stdatomic.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -13,7 +15,14 @@
 #define MPIMSG_TAG_TOSS     0UL
 #define MPIMSG_TAG_ITER     1UL
 
-inline static uint64_t test_point(struct drand48_data *state)
+static atomic_ullong point_inside = 0;
+static atomic_ullong point_total = 0;
+
+struct thread_ctx {
+    unsigned long long toss;
+};
+
+inline static unsigned long test_point(struct drand48_data *state)
 {
     double x, y;
     drand48_r(state, &x);
@@ -22,8 +31,10 @@ inline static uint64_t test_point(struct drand48_data *state)
     return (x * x + y * y) < 1.0;
 }
 
-static uint64_t pi_toss(uint64_t toss)
+static void *thread_main(void *_data)
 {
+    struct thread_ctx *data = (struct thread_ctx *)_data;
+
     unsigned short seed16v[3];
     FILE *rng_dev = fopen("/dev/urandom", "rb");
     if (rng_dev == NULL) {
@@ -36,10 +47,44 @@ static uint64_t pi_toss(uint64_t toss)
     struct drand48_data state;
     seed48_r(seed16v, &state);
 
-    uint64_t pi = 0;
-    for (size_t i = 0; i < toss; i++) {
+    unsigned long pi = 0;
+    for (size_t i = 0; i < data->toss; i++) {
         pi += test_point(&state);
     }
+
+    atomic_fetch_add(&point_inside, pi);
+    atomic_fetch_add(&point_total, data->toss);
+
+    return NULL;
+}
+
+static uint64_t pi_toss(uint64_t toss)
+{
+    size_t thread_cnt = get_nprocs();
+
+    pthread_t *thread_states = (pthread_t *)malloc(sizeof(pthread_t) * thread_cnt);
+    if (thread_states == NULL) {
+        abort();
+    }
+    struct thread_ctx *thread_data = (struct thread_ctx *)malloc(sizeof(struct thread_ctx) * thread_cnt);
+    if (thread_data == NULL) {
+        abort();
+    }
+
+    for (size_t t = 0; t < thread_cnt; t++) {
+        thread_data[t].toss = toss / thread_cnt;
+        thread_data[t].toss += (toss % thread_cnt) < t;
+
+        pthread_create(&thread_states[t], NULL, thread_main, &thread_data[t]);
+    }
+    for (size_t t = 0; t < thread_cnt; t++) {
+        pthread_join(thread_states[t], NULL);
+    }
+
+    free(thread_data);
+    free(thread_states);
+    
+    uint64_t pi = atomic_load(&point_inside);
 
     return pi;
 }
@@ -75,18 +120,18 @@ int main(int argc, char **argv)
             MPI_Send(&every_tosses, 1, MPI_UNSIGNED_LONG, i, MPIMSG_TAG_ITER, MPI_COMM_WORLD);
         }
 
-        uint64_t point_inside = pi_toss(every_tosses);
-        uint64_t point_total = every_tosses;
+        uint64_t pi = pi_toss(every_tosses);
+        uint64_t pp = every_tosses;
 
         for (int i = 1; i < world_size; i++) {
             MPI_Status status;
             uint64_t recvbuf[2];
             MPI_Recv(recvbuf, 2, MPI_UNSIGNED_LONG, i, MPIMSG_TAG_TOSS, MPI_COMM_WORLD, &status);
-            point_inside += recvbuf[0];
-            point_total += recvbuf[1];
+            pi += recvbuf[0];
+            pp += recvbuf[1];
         }
 
-        pi_result = ((double)point_inside / (double)point_total) * 4;
+        pi_result = ((double)pi / (double)pp) * 4;
 
         // --- DON'T TOUCH ---
         double end_time = MPI_Wtime();
